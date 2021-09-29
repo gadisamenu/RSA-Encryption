da1a492eaae364f5b66f1227599cbfaf9e0e347d
GAKfam <91586538+GAKfam@users.noreply.github.com>
2021-09-29 08:24:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import random,Rsa
 
+import Rsa
+
+#Added modular inverse function along with progress bar(percentage) for the encryption/decryption processes
 
 message = input("Enter the message you want to encrypt: ")
 
 secret = Rsa.Rsa()
 encrypted_message = secret.encrypt(message)
-print(encrypted_message)
 decrypted_message = secret.decrypt(encrypted_message)
 print(decrypted_message)
