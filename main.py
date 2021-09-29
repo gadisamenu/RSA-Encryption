0aeadf9ce87588d488ff5b2fd1b4164077877bfc
Bamlak <63541225+Bamlaku667@users.noreply.github.com>
2021-09-29 08:35:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 
 import Rsa
 
-#Added modular inverse function along with progress bar(percentage) for the encryption/decryption processes
-
-message = input("Enter the message you want to encrypt: ")
+#Removes the tedious generation of new prime numbers/keys everytime the program runs and Enables the generation of new keys at the user's will
 
 secret = Rsa.Rsa()
+message = input("Enter the message you want to encrypt: ").lower()
+command = input("Do you want to generate new keys(Y/N)?: ").lower()
+if command == 'y':
+  secret.generateKeys()
+elif command == 'n':
+  secret.initialize()
+else:
+  print("Invalid command!")
+  
 encrypted_message = secret.encrypt(message)
 decrypted_message = secret.decrypt(encrypted_message)
 print(decrypted_message)
